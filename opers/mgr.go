5f21534151175a37d9f0fc942f6df5d5cd3f7dfe
Jorge Florian <jorge.floriannoriega@aiven.io>
2024-06-26 10:55:26 +0200

    refactor iteration package

--- opers/mgr.go
+++ opers/mgr.go
@@ -1892,7 +1892,7 @@ func (sm *streamManager) streamMetaIterator(startKey []byte, endKey []byte) iter
 			})
 		}
 	}
-	return iteration.NewStaticIterator(entries)
+	return iteration.NewIteratorAdapter(iteration.NewStaticIterator(entries))
 }
 
 func (sm *streamManager) StreamMetaIteratorProvider() *StreamMetaIteratorProvider {
